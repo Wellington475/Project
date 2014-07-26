85f4fb9c5bbc3b1281a18e5b034f64e2dc02a214
wellignton santos <wellington30071997@gmail.com>
2014-07-26 16:32:57 -0300

    project new

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE >
+<html>
+	<head>
+		<title>
+			Cloud Game
+		</title>
+		<meta charset='utf-8'>
+		<link rel="stylesheet" type="text/css" media="all" href="static/css/bootstrap.css"/>
+	</head>
+	<body>
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#"><img src="static/img/Logo.png" style="height:150px; margin-top:-65px;"></a>
+	        </div>
+	        <div class="navbar-collapse collapse">
+	          <form class="navbar-form navbar-right" role="form">
+	            <div class="form-group">
+	              <input type="text" placeholder="Email" class="form-control">
+	            </div>
+	            <div class="form-group">
+	              <input type="password" placeholder="Password" class="form-control">
+	            </div>
+	            <button type="submit" class="btn btn-success">Entrar</button>
+	          </form>
+	        </div>
+	      </div>
+	    </div>
+	    <div class="jumbotron">
+	      <div class="container">
+	        <h1>Cloud Game</h1>
+	        <p> Cloud Game é o melhor destino para os jogadores ou criadores de jogos online, aqui é o melhor lugar para compartilhar sua experiência com games na web. No Cloud Game você pode construir e compartilhar jogos juntos de seus amigos.</p>
+	        <p><a class="btn btn-primary btn-lg" role="button">Cadastrar-se</a></p>
+	      </div>
+	    </div>
+		<div class="container marketing">
+	      <div class="row">
+	        <div class="col-lg-4" style="text-align: center;">
+	          <img class="img-circle" src="static/img/code.png" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+	          <h2>Open-source</h2>
+	          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+	        </div>
+	        <div class="col-lg-4" style="text-align: center;">
+	          <img class="img-circle" src="static/img/user.png" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+	          <h2>Social</h2>
+	          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+	        </div>
+	        <div class="col-lg-4" style="text-align: center;">
+	          <img class="img-circle" src="static/img/game.png" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+	          <h2>Game</h2>
+	          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+	        </div>
+	      </div>
+	      <hr class="featurette-divider">
+	      <div class="row featurette">
+	        <div class="col-md-7">
+	          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+	          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+	        </div>
+	        <div class="col-md-5">
+	          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="500x500" src="static/img/computer.jpg">
+	          <img src="static/img//page.png" style="margin-top: -70%; margin-left: 4%; height:200px; width:420px;">
+	        </div>
+	      </div>
+	      <hr class="featurette-divider">
+	      <div class="row featurette">
+	        <div class="col-md-5">
+	          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="500x500" src="static/img/ferramenta.jpg">
+	        </div>
+	        <div class="col-md-7">
+	          <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+	          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+	        </div>
+	      </div>
+	      <hr class="featurette-divider">
+	      <div class="row featurette"style="text-align: center;">
+	        <div align="center">
+	          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+	          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+	        </div>
+	        <div align="center">
+	          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="500x500" src="static/img/banner_games.png">
+	        </div>
+	      </div>
+	    </div>
+		<div class="footer" style="margin-top:50px; background-color: #1a1d30;">
+			<div class="container">
+		    	<img src="static/img/Logo.png" style="height: 100px; float: left;">
+		    	<p style="color:#fff; float:left; margin-top: 3.5%; margin-left: 30%"> © Cloud Game 2014</p>
+			</div>
+		</div>
+	</body>
+</html>
